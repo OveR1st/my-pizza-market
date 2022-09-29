d19a9776aeb3d8550b380726d408e49545090ec6
Dmitry <315overdose@gmail.com>
2022-09-29 19:10:53 +0300

    init husky with test

---
+++ src/app.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import {Provider} from "react-redux";
+import { setupStore } from './store/store';
+const store = setupStore()
+describe('TEST APP', () => {
+  test('renders App', () => {
+    render(  
+    <MemoryRouter>
+      <Provider store={store}>
+        <App/>
+      </Provider>
+  </MemoryRouter>);
+  });
+
+})
+
